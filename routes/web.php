332bd9a60efb11ce922e5e80059d19a4e2d88ed3
nikhilastound <nikhil.astound@gmail.com>
2017-07-10 16:42:11 +0530

    create login and register form.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('test', function () {
+Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
